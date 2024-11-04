139ce165c60e6fe3647a452433e562a1e3e90969
typescript project <farhanakhanzai14@gmail.com>
2024-11-04 18:27:37 +0500

    Removed ignored files from tracking

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,131 @@
+"use client"
+
+ 
+import React from "react";
 import Image from "next/image";
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div >
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+
+    <div className="bg-blue-500 w-[100%] flex justify-left  text-white  h-16 gap-2 mr-4 font-bold relative ">
+      
+      
+    <Image src={require("./../../public/images/logo.png")} alt="logo" className="w-[6rem] h-fit ml-10 mt-4 " ></Image>
+
+  <h1 className="  font-semibold text-white  text-2xl  mt-5 m-auto">Tuition Free Education Program on Latest Technologies</h1>
+ 
+ 
+ <h3 className="flex mt-5  mr-5  "> Home </h3> 
+ <h3 className="flex mt-5  mr-5">Apply </h3>
+ <h3 className="flex mt-5  mr-5 ">Jobs</h3>
+ <h3 className="flex mt-5 mr-5">Result </h3>
+ <h3 className="flex mt-5   mr-5">Courses </h3>
+ 
+ </div>
+ <nav/>
+
+<div>
+ <Image src={require("./../../public/images/govhouse.jpg")}alt="image" className=" relative p-5 opacity-10 h-88 w-full  " ></Image>
+
+ <Image src={require("./../../public/images/cover.1d863e39.png")}alt="image" className="   flex  absolute inset-0 ml-auto py-32 justify-end items-end pl-96 w-[70rem] h-[140%]  " ></Image>
+ 
+<br></br>
+
+
+<div>
+  <h1 className="absolute inset-0 object-contain mt-52 ml-16 font-bold text-7xl text-blue-500">
+  Governor Sindh
+ </h1>
+
+ <h3 className=" absolute inset-0 object-contain mt-72 ml-16 font-semibold text-5xl text-blue-900 ">
+
+ Kamran Khan Tessori
+ </h3>
+
+ <h3 className=" absolute inset-0 object-contain mt-80 p-14 ml-15 font-semibold text-5xl text-sky-500">
+Certified Cloud
+</h3>
+<h3 className=" absolute inset-0 object-contain mt-80 p-28 -ml-16 font-semibold text-5xl text-sky-500">
+Applied Generative AI
+</h3>
+<h3 className=" absolute inset-0 object-contain mt-80 p-40 -ml-28 font-semibold text-5xl text-sky-500">
+Engineer (GenEng)
+</h3>
+<h3 className=" absolute inset-0 object-contain mt-80 p-60 -ml-48 font-bold text-2xl text-blue-800">
+Earn up to $5,000 / month
+</h3>
+<h3 className=" absolute inset-0 object-contain mt-80 p-72 -ml-60 font-bold text-2xl text-blue-800">
+Now admissions are open in <br></br> Hyderabad
+
+
+<div className="flex flex-col h-screen justify-start w-52 ml-0 mt-8 ">
+  <button className="px-2 py-2 bg-blue-500 text-white rounded-md font-semibold">
+    Apply Now
+  
+  </button>
+  <br></br>
+  <div className="flex flex-col  inset-0 absolute justify-center items-center mt-52 mr-44 text-4xl font-sans ">
+  562,143
+ 
+</div>
+<div className="flex flex-col text-center ml-80 font-semibold absolute mt-7 text-lg justify-center font-sans ">
+Accepted Applications
+</div>
+</div>
+</h3> 
+<div className="text-3xl text-center font-bold text-blue-900 p-10">
+Certified Cloud Applied Generative AI Engineer (GenEng) and Solopreneur <br></br>Developing Billion-Dollar Valued Developers and Solopreneurs
+
+<p className="text-xl text-justify  font-thin"><br>
+</br>
+The pace of technological change is accelerating, big players like Microsoft, Amazon, Google, and OpenAI are winning by providing infrastructure, large AI foundation models, frameworks, 3D Metaverse experiences, and massive distribution networks. Solopreneurs trained in this program will win by automating work typically outsourced to employees, by directly connecting to customers by eliminating the middleman, and by developing vertical metaverses, thus paving the way for the first billion-dollar valued solopreneur businesses. This program has the objective to train this new breed of billion-dollar solopreneurs. These solopreneurs will adopt the ultra-lean business model and work independently and will not need to hire employees or other team members.
+</p>
+
+<div className="p-5 flex mr-14  ">
+<Image src={require("./../../public/images/manweb.jpg")}alt="image grid  ">
+
+</Image>
+<div className="flex   items-left pl-20 ">
+<Image src={require("./../../public/images/imageWeb.jpg")}alt="image grid  ">
+
+</Image>
+</div>
+<div>
+</div>
+</div>
+
+
+</div>
+
+</div>
+
+
+
+</div>
+
+</div>
+ 
+
+
+ 
+ 
+
+
+
+
+
+
+
+
+    
   );
 }
+
+
+
+
+ 
